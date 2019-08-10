72954e05f0457634b2b74b18e21dd6628fb3b697
Matheus Mota <smota.matheus@gmail.com>
2019-08-10 15:54:13 -0300

    mqtt examples

--- components/message/publisher-uol.py
+++ components/message/publisher-uol.py
@@ -9,7 +9,7 @@ def on_publish(client,userdata,result):             #create function for callbac
 
 client1 = paho.Client("publisher{0}".format(random.randint(0,99999999)))                           #create client object
 #client1.on_publish = on_publish                          #assign function to callback
-if(not client1.connect(broker,port)):                     #establish connection
+if(not client1.connect(broker,port)):                              #establish connection
     print("Connected.")
 
 
@@ -19,23 +19,23 @@ import random
 import feedparser
 import json
 
-authors  = ['Asdrubal', 'Luizadino','Yarapy']
+authors = ['Asdrubal', 'Luizadino','Yarapy']
 approved = [True, False, False, False, False]
 
 print("publishing...")
 
 while(True):
-    d = feedparser.parse('http://pox.globo.com/rss/g1/')
+    d = feedparser.parse('http://rss.uol.com.br/feed/noticias.xml')
     for post in d.entries:
         item ={
                'title':    post.title,
                'summary':  post.summary,
                'link':     post.link,
-               'source':   'g1',
+               'source':   'uol',
                'author':   random.choice (authors),
                'approved': random.choice (approved)
               }
 
         client1.publish(pub_topic,json.dumps(item)) 
-        time.sleep(random.randint(3,8))
+        time.sleep(random.randint(3,7))
 print("Finished publishing!")
