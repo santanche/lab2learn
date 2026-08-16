7795d35b3fee93070a7fc6416d20724f2cef70bb
André Santanchè <santanche@gmail.com>
2026-08-16 13:52:20 -0300

    refactor (data): Case Teaching in Medicine transferred to NLP2Learn

--- data/case-teaching-cabot/extractor/converter/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',                    views.index,    name='index'),
-    path('upload/',             views.upload,   name='upload'),
-    path('status/<str:job_id>/', views.status,  name='status'),
-    path('result/<str:job_id>/', views.result,  name='result'),
-]
